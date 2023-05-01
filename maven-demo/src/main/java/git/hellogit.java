eb17d7bd8a0d0aee8c0dc2fbabc16bfd885151a0
hjj-766 <646774782@qq.com>
2023-05-01 22:04:25 +0800

    分支添加

--- maven-demo/src/main/java/git/hellogit.java
+++ maven-demo/src/main/java/git/hellogit.java
@@ -5,6 +5,7 @@ public class hellogit {
         System.out.println("hellogit");
         System.out.println("hellogit001");
         System.out.println("创建的dev分支");
+        System.out.println("分支添加");
     }
 
 }
